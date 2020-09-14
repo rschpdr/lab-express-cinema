055707e6b457b2aa41fb179dc5e14d38a723cbda
rschpdr <pedro.resch@gmail.com>
2020-09-14 17:41:11 -0300

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+// GET Movie List
+router.get("/movies", async (req, res) => {
+  try {
+    const result = await Movie.find();
+
+    res.render("movies", { movies: result });
+  } catch (err) {
+    console.error(err);
+    throw new Error(err);
+  }
+});
+
+// GET Movie detail
+router.get("/movie/:id", async (req, res) => {
+  console.log("PARAMS =>", req.params);
+
+  try {
+    const result = await Movie.findOne({ _id: req.params.id });
+
+    res.render("movieDetail", result);
+  } catch (err) {
+    console.error(err);
+    throw new Error(err);
+  }
+});
+
+// GET Movie detail
+router.get("/movie", async (req, res) => {
+  console.log("PARAMS =>", req.params);
+
+  try {
+    const result = await Movie.findOne({ _id: req.params.id });
+
+    res.render("movieDetail", result);
+  } catch (err) {
+    console.error(err);
+    throw new Error(err);
+  }
+});
 
 module.exports = router;
