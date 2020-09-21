b323bb102e532e10d223474861da8674a9f9ef64
Julia Foresti <juliajforesti@gmail.com>
2020-09-21 09:20:16 -0300

    changed get route home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get("/", (req, res, next) => res.render("index"));
+router.get("/home", (req, res, next) => res.render("index"));
 
 // GET Movie List
 router.get("/movies", async (req, res) => {
